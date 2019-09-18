a6c6b744c4d32dffc820528f1830692321ac08be
Your Name <you@example.com>
2019-09-18 12:45:30 -0500

    agrego estilos para mejorar el diseno de mis habilidades generales

--- js/BD.js
+++ js/BD.js
@@ -34,7 +34,7 @@ let JsonInicio = {
     Contenido:[
         {
             pregunta:"¿Que hago?",
-            respuesta:"En todo este tiempo  eh adquirido habilidades en : <ul><li><span class='nomHablidad'>Frond-End</span> <span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.frondEnd+"'></span></span></li><li> <span class='nomHablidad'>Back-End</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.backEnd+"'></span></span></li><li><span class='nomHablidad'> Android(Java)</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.android+"'></span></span></li><li>Desktop(VB .NET) <span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.descktop+"'></span></span></li></ul>"
+            respuesta:"En todo este tiempo  eh adquirido habilidades en : <ul><li><span class='nomHablidadGeneral'>Frond-End</span> <span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.frondEnd+"'></span></span></li><li> <span class='nomHablidadGeneral'>Back-End</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.backEnd+"'></span></span></li><li><span class='nomHablidadGeneral'> Android(Java)</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.android+"'></span></span></li><li><span class='nomHablidadGeneral'> Desktop(VB .NET) </span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.descktop+"'></span></span></li></ul>"
         },
         {
             pregunta:"¿Donde estoy?",
