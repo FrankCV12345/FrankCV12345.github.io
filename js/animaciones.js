e2f31672b024de935a4516eb8ac4bf21a57092db
Your Name <you@example.com>
2019-09-19 16:11:16 -0500

    hago mejoras en el disenio y la ortografia

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,4 +1,3 @@
-//import { JsonSobreMi,JsonInicio,JsonAportes,JsonContacto,JsonPortaFolio } from './BD.js'
 let  iconbarras = document.getElementById("iconBarras")
 let  lineasDentrodeiconBarras = iconbarras.children
 let contMenu  = document.getElementById("contMenu")
@@ -20,19 +19,19 @@ let JsonSobreMi = {
     titulo:"Sobre mi",
     Contenido:[
         {
-            pregunta:"¿Por que escoji ser desarrollador?",
-            respuesta:"Me gusta crear , me siento lleno cuando veo  que el fruto de mi esfuerso es de utilidad para las personas."
+            pregunta:"¿Por que escogí ser desarrollador?",
+            respuesta:"Me gusta crear , me siento lleno cuando veo  que el fruto de mi esfuerzo es de utilidad para las personas."
         },
         {
-            pregunta:"¿En que area me gustaria desarrollarme?",
+            pregunta:"¿En que área me gustaría desarrollarme?",
             respuesta:"BackEnd , me  parece un mundo muy entretenido pero en general todo a lo que conlleve programar."
         },
         {
             pregunta:"¿Mi leguaje favorito?",
-            respuesta:"No lo tengo :( , hasta ahora todo lo que probe  tiene sus ventajas y desventajas pero considero que toda tecnologia tiene su utlidad en el momento adecuado. </br> Aunque de ves encuando me gusta jugar en el FrondEnd , desarrolle esta pagina con Html,CSS y JS sin frameworks( me gusta probarme que aun no pierdo mi toque ;) )"
+            respuesta:"No lo tengo , hasta ahora todo lo que probé  tiene sus ventajas y desventajas pero considero que toda tecnología tiene su utilidad en el momento adecuado.</br>Aunque de ves en cuando me gusta jugar en el FrondEnd , desarrollé esta pagina con Html,CSS y JS( me gusta probarme que aun no pierdo mi toque ;) )"
         },
         {
-            pregunta:"Mis habilidades Tecnicas",
+            pregunta:"Mis habilidades Técnicas",
             respuesta:"<ul>"+ItemParalistaDeHabilidades("java")+ItemParalistaDeHabilidades("php") +ItemParalistaDeHabilidades("vb")+ItemParalistaDeHabilidades("js")+ItemParalistaDeHabilidades("html")+
             ItemParalistaDeHabilidades("css")+ItemParalistaDeHabilidades("mySql")+ItemParalistaDeHabilidades("sqlServer")+ItemParalistaDeHabilidades("NET")+ItemParalistaDeHabilidades("laravel")+ItemParalistaDeHabilidades("jQuery")+
             ItemParalistaDeHabilidades("git")+ItemParalistaDeHabilidades("VueJs")+ItemParalistaDeHabilidades("linux")+ItemParalistaDeHabilidades("Android")
@@ -44,25 +43,28 @@ let JsonInicio = {
     titulo:"Inicio",
     Contenido:[
         {
+            pregunta:"Franklin yoan campos vilchez ",
+            respuesta:"Programador Junior."
+        },{
             pregunta:"¿Que hago?",
-            respuesta:"En todo este tiempo  eh adquirido habilidades en : <ul><li><span class='nomHablidadGeneral'>Frond-End</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.frondEnd+"'></span></span></li><li> <span class='nomHablidadGeneral'>Back-End</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.backEnd+"'></span></span></li><li><span class='nomHablidadGeneral'> Android(Java)</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.android+"'></span></span></li><li><span class='nomHablidadGeneral'> Desktop(VB .NET) </span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.descktop+"'></span></span></li></ul>"
+            respuesta:"He adquirido habilidades en : <ul><li><span class='nomHablidadGeneral'>Frond-End</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.frondEnd+"'></span></span></li><li> <span class='nomHablidadGeneral'>Back-End</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.backEnd+"'></span></span></li><li><span class='nomHablidadGeneral'> Android(Java)</span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.android+"'></span></span></li><li><span class='nomHablidadGeneral'> Desktop(VB .NET) </span><span class='cont-med-habilidad'><span style='width:"+hablidaddes.generales.descktop+"'></span></span></li></ul>"
         },
         {
-            pregunta:"¿Donde estoy?",
-            respuesta:"A la fecha 16/09/2019, estoy participando en la cominidad de Hack space, Aprendiendo .NET framework."
+            pregunta:"¿Dónde estoy?",
+            respuesta:"A la fecha 16/09/2019, estoy participando en la comunidad de <a href='https://www.facebook.com/HackSpacePeru' target='_blank'>Hack space</a>, Aprendiendo .NET framework y un poco de Data Science."
         }
     ]
 }
-let JsonAportes ={
+let JsonEstudios ={
     titulo:"Estudios",
     Contenido:[
         {
             pregunta:"IDAT",
-            respuesta:"<ul><li>Informatica</li></ul>"
+            respuesta:"<ul><li>Informática</li></ul>"
         },
         {
             pregunta:"Udemy",
-            respuesta:"<ul><li>VueJS + Vuetify </li><li>Laravel</li><li>Android</li></ul>"
+            respuesta:"<ul><li>VueJS + Vuetify </li><li>Laravel</li><li>Android (JAVA)</li></ul>"
         },
         {
             pregunta:"CEPS UNI",
@@ -84,7 +86,7 @@ let JsonCapturasPortafolio = {
     trabajos:
     [
         {urlCaptura:urlCapturasTrabajo+"capturavistacontrolFER.jpg",Descripcion:"App de control para Robot, este fue un  proyecto de un app android y una serie de aplicaciones de escritorio las cuales en conjunto sirven para la transmisión de audio y video  para la pc del robot , además de esto el App envía órdenes de movimiento a la pc y la pc al arduino . Las tecnologías que use fue java desktop , java en android, php(para hacer un login) <a href='https://drive.google.com/open?id=115PAVWLudRaWyW-tye8C0_w7t7jlM9NO' target='_blank'>Video demo</a>."},
-        {urlCaptura:urlCapturasTrabajo+"captura-lista-cotizaciones-Plataformapraco.png",Descripcion:"Plataforma praco es un sistema web en el cual se puede generar cotizaciones , ordenes de compra  y cartas  de garantía para producto. Este fue desarrollado con laravel 5.5 y VueJS en este caso yo me encargue del API Rest y de darle mantenimiento al FrontEnd "},
+        {urlCaptura:urlCapturasTrabajo+"captura-lista-cotizaciones-Plataformapraco.png",Descripcion:"Plataforma praco es un sistema web en el cual se puede generar cotizaciones , ordenes de compra  y cartas  de garantía para producto. Este fue desarrollado con laravel 5.5 y VueJS en este caso yo me encargue del API Rest y de darle mantenimiento al FrontEnd."},
         {urlCaptura:urlCapturasTrabajo+"STACK-MANAGER.PNG",Descripcion:"PICK , es un sistema desktop que sirve para tratar archivos CSV los cuales luego serán usados para hacer circuitos impresos, este proyecto está desarrollado en VB sobre el framework .NET."},
         {urlCaptura:urlCapturasTrabajo+"modifica-ambiente-SRD.PNG",Descripcion:"SRD , es  un sistema web para registrar datos de ambientes en edificios para luego ser analizados . Este proyecto fue desarrollado con VueJS y firebase."}        
     ],
@@ -132,7 +134,7 @@ function mover(){
                             llenaContenido(JsonInicio)
                         break;
                     case 1:
-                            llenaContenido(JsonAportes)
+                            llenaContenido(JsonEstudios)
                         break;
                     case 2:
                             llenaContenido(JsonContacto)
